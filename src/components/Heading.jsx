27e9116f405b55c17dcf287d9be40edfe3ccb3b2
Balaguru0414 <balag0414@gmail.com>
2023-03-08 20:44:16 +0530

    finish components

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const Heading = function () {
+/*const Heading = function () {
   return <h1>My name is Bala Guru</h1>
+}
+
+export default Heading;
+*/
+//-----------------------------------------------------------------------------------------------
+const Heading = function() {
+
+const date = new Date();
+const curTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color : ""
+}
+
+if(curTime < 12) {
+  greeting = 'Good Morning';
+  customStyle.color = 'red';
+} else if(curTime < 18) {
+  greeting = 'Good Afternoon';
+  customStyle.color = 'green';
+} else {
+  greeting = 'Good Night';
+  customStyle.color = 'blue';
+}
+
+return <h1 className="heading" style={customStyle}>{greeting}</h1>
 }
 
 export default Heading;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const add = function (num1,num2) {
+  return num1 + num2;
+}
+
+const multiply = function (num1,num2) {
+  return num1 * num2;
+}
+
+const subtract = function (num1,num2) {
+  return num1 - num2;
+}
+
+const divide = function (num1,num2) {
+  return num1 / num2;
+}
+
 const List = function () {
   return <ul>
-    <li> java </li>
-    <li> html </li>
-    <li> css </li>
+    <li> {add(1,2)} </li>
+    <li> {multiply(2,3)} </li>
+    <li> {subtract(7,2)} </li>
+    <li> {divide(5,2)} </li>
   </ul>
 }
 
